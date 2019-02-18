400ac48c47a55beadb28cc4ec3e6819e18a59fff
Sara Patterson <sarap44@uw.edu>
2019-02-17 16:20:50 -0800

    adapting steps and new stimuli

---
+++ src/main/matlab/+edu/+washington/+riekelab/+patterson/+previews/FullStimuliPreview.m
@@ -0,0 +1,93 @@
+classdef FullStimuliPreview < symphonyui.core.ProtocolPreview
+    % Displays cell array of stimuli on a 2D plot
+    % 
+    % Stimuli are concatenated along the x-axis to show full epoch group
+    % 
+    % History:
+    %   17Feb2019 - SSP
+    % ---------------------------------------------------------------------
+
+    properties 
+        createStimuliFcn
+        colors
+    end
+
+    properties (Access = private)
+        log
+        axes
+    end
+
+    methods 
+        function obj = FullStimuliPreview(panel, createStimuliFcn, colors)
+            % Constructs a StimuliPreview on the given panel with the given
+            % stimuli. createStimuliFcn should be a callback function that 
+            % creates a cell array of stimuli.
+            
+            obj@symphonyui.core.ProtocolPreview(panel);
+            obj.createStimuliFcn = createStimuliFcn;
+
+            if nargin < 3
+                obj.colors = [];
+            else
+                assert(length(colors) == 3, 'Colors must be Nx3 matrix');
+                obj.colors = colors;
+            end
+
+            obj.log = log4m.LogManager.getLogger(class(obj));
+            obj.createUi();
+        end
+
+        function createUi(obj)
+            obj.axes = axes( ...
+                'Parent', obj.panel, ...
+                'FontName', get(obj.panel, 'DefaultUicontrolFontName'), ...
+                'FontSize', get(obj.panel, 'DefaultUicontrolFontSize'), ...
+                'XTickMode', 'auto'); %#ok<CPROP>
+            xlabel(obj.axes, 'sec');
+            obj.update();
+        end
+
+                
+        function update(obj)
+            cla(obj.axes);
+            
+            try
+                stimuli = obj.createStimuliFcn();
+            catch x
+                cla(obj.axes);
+                text(0.5, 0.5, 'Cannot create stimuli', ...
+                    'Parent', obj.axes, ...
+                    'FontName', get(obj.panel, 'DefaultUicontrolFontName'), ...
+                    'FontSize', get(obj.panel, 'DefaultUicontrolFontSize'), ...
+                    'HorizontalAlignment', 'center', ...
+                    'Units', 'normalized');
+                obj.log.debug(x.message, x);
+                return;
+            end
+
+            if ~iscell(stimuli) && isa(stimuli, 'symphonyui.core.Stimulus')
+                stimuli = {stimuli};
+            end
+
+            if isempty(obj.colors) || size(obj.colors, 1) < numel(stimuli)
+                obj.colors = edu.washington.riekelab.patterson.utils.pmkmp(numel(stimuli), 'CubicL');
+            end
+
+            ylabels = cell(1, numel(stimuli));
+            xStart = 0;
+            for i = 1:numel(stimuli)
+                [y, units] = stimuli{i}.getData();
+                x = (1:numel(y)) / stimuli{i}.sampleRate.quantityInBaseUnits;
+                
+                x = x + xStart;
+                xStart = max(x);
+                
+                line(x, y, 'Parent', obj.axes,...
+                    'LineWidth', 0.8, 'Color', obj.colors(i, :));
+                ylabels{i} = units;
+            end
+            ylabel(obj.axes, strjoin(unique(ylabels), ', '),... 
+                'Interpreter', 'none');
+        end
+    end
+end
---
+++ src/main/matlab/+edu/+washington/+riekelab/+patterson/+protocols/AdaptingPyramid.m
@@ -0,0 +1,156 @@
+classdef AdaptingPyramid < edu.washington.riekelab.protocols.RiekeLabProtocol
+   
+    properties
+        led
+        
+        stepTime = 1000         % Duration of adapting step
+        
+        baseMagnitude = 0       % Magnitude of LED at first step (V)
+        topMagnitude = 5        % Magnitude of LED at peak step (V)
+        
+        numSteps = 3            % Number of pyramid steps from base to top
+        onlineAnalysis = 'none' % Online analysis type
+        
+        interpulseInterval = 0  % Duration between pulses (ms)
+        amp                     % Input amplifier
+    end
+    
+    properties (Dependent, SetAccess = private)
+        amp2                    % Secondary amplifier
+    end
+    
+    properties (Dependent, Hidden = true)
+        totalEpochs
+    end
+    
+    properties (Hidden)
+        ledType
+        ampType
+        onlineAnalysisType = symphonyui.core.PropertyType('char', 'row',...
+            {'none', 'extracellular', 'exc', 'inh'});
+    end
+    
+    methods
+        function didSetRig(obj)
+            didSetRig@edu.washington.riekelab.protocols.RiekeLabProtocol(obj);
+            
+            [obj.led, obj.ledType] = obj.createDeviceNamesProperty('LED');
+            [obj.amp, obj.ampType] = obj.createDeviceNamesProperty('Amp');
+        end
+        
+        function d = getPropertyDescriptor(obj, name)
+            d = getPropertyDescriptor@edu.washington.riekelab.protocols.RiekeLabProtocol(obj, name);
+
+            if strncmp(name, 'amp2', 4) && numel(obj.rig.getDeviceNames('Amp')) < 2
+                d.isHidden = true;
+            end
+        end
+        
+        function p = getPreview(obj, panel)
+            p = edu.washington.riekelab.patterson.previews.FullStimuliPreview(...
+                panel, @()createPreviewStimuli(obj));
+            
+            function s = createPreviewStimuli(obj)
+                s = cell(obj.totalEpochs, 1);
+                for i = 1:obj.totalEpochs
+                    s{i} = obj.createLedStimulus(i);
+                end
+            end
+        end
+        
+        function prepareRun(obj)
+            prepareRun@edu.washington.riekelab.protocols.RiekeLabProtocol(obj);
+            colors = edu.washington.riekelab.patterson.utils.pmkmp(obj.numSteps, 'CubicL');
+
+            if numel(obj.rig.getDeviceNames('Amp')) < 2
+                obj.showFigure('symphonyui.builtin.figures.ResponseFigure',...
+                    obj.rig.getDevice(obj.amp));
+                obj.showFigure('edu.washington.riekelab.patterson.figures.MeanResponseFigure',...
+                    obj.rig.getDevice(obj.amp), 'groupBy', {'stepAmplitude'},...
+                    'recordingType', obj.onlineAnalysis, 'sweepColor', colors);
+                if strcmp(obj.onlineAnalysis, 'extracellular')
+                    obj.showFigure('edu.washington.riekelab.patterson.figures.SpikeStatisticsFigure',...
+                        obj.rig.getDevice(obj.amp));
+                else
+                    obj.showFigure('symphonyui.builtin.figures.ResponseStatisticsFigure',...
+                        obj.rig.getDevice(obj.amp), {@mean, @var});
+                end
+            else
+                obj.showFigure('symphonyui.builtin.figures.DualResponseFigure',...
+                    obj.rig.getDevice(obj.amp), obj.rig.getDevice(obj.amp2));
+            end
+            
+            device = obj.rig.getDevice(obj.led);
+            device.background = symphonyui.core.Measurement(...
+                obj.baseMagnitude, device.background.displayUnits);
+        end
+        
+        function [stim, stepAmplitude] = createLedStimulus(obj, epochNum)
+            
+            allSteps = linspace(obj.baseMagnitude, obj.topMagnitude, obj.numSteps);
+            allSteps = [allSteps, fliplr(allSteps(1:end-1))];
+            
+            stepAmplitude = allSteps(epochNum);
+            
+            gen = symphonyui.builtin.stimuli.PulseGenerator();
+            gen.preTime = 0;
+            gen.stimTime = obj.stepTime;
+            gen.tailTime = 0;
+            gen.amplitude = stepAmplitude;
+            gen.mean = obj.baseMagnitude;
+            gen.sampleRate = obj.sampleRate;
+            gen.units = obj.rig.getDevice(obj.led).background.displayUnits;
+            
+            stim = gen.generate();
+        end
+        
+        function prepareEpoch(obj, epoch)
+            prepareEpoch@edu.washington.riekelab.protocols.RiekeLabProtocol(obj, epoch);
+            
+            epochNum = obj.numEpochsPrepared;
+            [stim, stepAmplitude] = obj.createLedStimulus(epochNum);
+            
+            epoch.addParameter('stepAmplitude', stepAmplitude);
+            epoch.addStimulus(obj.rig.getDevice(obj.led), stim);
+
+            epoch.addResponse(obj.rig.getDevice(obj.amp));
+            if numel(obj.rig.getDeviceNames('Amp')) >= 2
+                epoch.addResponse(obj.rig.getDevice(obj.amp2));
+            end
+        end        
+        
+        function prepareInterval(obj, interval)
+            prepareInterval@edu.washington.riekelab.protocols.RiekeLabProtocol(obj, interval);
+            
+            device = obj.rig.getDevice(obj.led);
+            interval.addDirectCurrentStimulus(device, device.background,...
+                obj.interpulseInterval, obj.sampleRate);
+        end
+        
+        function tf = shouldContinuePreparingEpochs(obj)
+            tf = obj.numEpochsPrepared < obj.totalEpochs;
+        end
+        
+        function tf = shouldContinueRun(obj)
+            tf = obj.numEpochsCompleted < obj.totalEpochs;
+        end
+    end
+
+    % Dependent set/get methods
+    methods
+        
+        function value = get.totalEpochs(obj)
+            value = 2 * obj.numSteps - 1;
+        end
+        
+        function a = get.amp2(obj)
+            amps = obj.rig.getDeviceNames('Amp');
+            if numel(amps) < 2
+                a = '(None)';
+            else
+                i = find(~ismember(amps, obj.amp), 1);
+                a = amps{i};
+            end
+        end
+    end
+end
